ee9fa00c0a17334264c8d5c266f9991251e238f3
msKaleb <sjmikel@gmx.es>
2024-09-12 20:10:15 +0200

    starting calculate()

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const calculatorKeys = Array.from(document.getElementsByClassName("key"));
 const display = document.getElementById("calculator__display");
+const res = document.getElementById("res");
 
 $(document).ready(function() {
 
@@ -8,6 +9,13 @@ $(document).ready(function() {
 let operationList = [];
 let isDecimal = false;
 
+const operatorToFunction = {
+  "+": (x, y) => x + y,
+  "-": (x, y) => x - y,
+  "×": (x, y) => x * y,
+  "÷": (x, y) => x / y,
+}
+
 const addOperator = (operator, operand) => {
   operationList.push(operand, operator);
   display.value = operator;
@@ -15,8 +23,15 @@ const addOperator = (operator, operand) => {
 }
 
 const calculate = () => {
+  res.textContent = "calculate() from function";
+  
+  // first loop for high priority operators
+  // TODO: update operationList array with new numbers, maybe removing that item
   for (let i = 0; i < operationList.length; i++) {
-    
+    if (operationList[i] == "×" || operationList[i] == "÷") {
+      res.textContent = "calculate() from loop";
+      res.textContent = operatorToFunction[operationList[i]](operationList[i - 1], operationList[i + 1]);
+    }
   }
 }
 
@@ -29,18 +44,20 @@ calculatorKeys.forEach((key) => {
       display.value = "0";
     }*/
    if (key.classList.contains("number")) {
-    display.value === "0" || isNaN(Number(display.value)) ? display.value = content : display.value += content;
-   } else if (key.classList.contains("ac")) {
-    display.value = "0";
-    operationList = [];
-   } else if (key.classList.contains("operator")) {
-    addOperator(content, display.value);
-    console.log(operationList);
-   } else if (key.classList.contains("decimal") && !isDecimal) {
-    display.value += ".";
-    isDecimal = true;
-   } else if (key.classList.contains("equals")) {
-    
+     display.value === "0" || isNaN(Number(display.value)) ? display.value = content : display.value += content;
+    } else if (key.classList.contains("ac")) {
+      display.value = "0";
+      operationList = [];
+    } else if (key.classList.contains("operator")) {
+      addOperator(content, display.value);
+      console.log(operationList);
+    } else if (key.classList.contains("decimal") && !isDecimal) {
+      display.value += ".";
+      isDecimal = true;
+    } else if (key.classList.contains("equals")) {
+      operationList.push(display.value);
+      res.textContent = isNaN(operationList[operationList.length - 1]);
+      isNaN(operationList[operationList.length - 1]) ? alert("Invalid expression") : calculate();
    }
   });
 });
