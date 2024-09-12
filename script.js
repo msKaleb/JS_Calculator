8636ef5cade5a9df57ef8ef7c9aa124e46686690
msKaleb <sjmikel@gmx.es>
2024-09-12 18:38:42 +0200

    second commit

--- script.js
+++ script.js
@@ -5,17 +5,43 @@ $(document).ready(function() {
 
 });
 
-// TODO: maybe use action=addNumber and action=calculate, etc in HTML
+let operationList = [];
+let isDecimal = false;
+
+const addOperator = (operator, operand) => {
+  operationList.push(operand, operator);
+  display.value = operator;
+  // console.log(operator);
+}
+
+const calculate = () => {
+  for (let i = 0; i < operationList.length; i++) {
+    
+  }
+}
 
 calculatorKeys.forEach((key) => {
   key.addEventListener("click", (value) => {
-    const num = value.target.textContent;
-    if (!isNaN(Number(num))) {
-      console.log(num);
-      display.value !== "0" ? display.value += num : display.value = num;
+    const content = value.target.textContent;
+    /*if (!isNaN(Number(num))) {
+      display.value === "0" ? display.value = num : display.value += num;
     } else if (value.target.textContent === "AC") {
       display.value = "0";
-    }
+    }*/
+   if (key.classList.contains("number")) {
+    display.value === "0" || isNaN(Number(display.value)) ? display.value = content : display.value += content;
+   } else if (key.classList.contains("ac")) {
+    display.value = "0";
+    operationList = [];
+   } else if (key.classList.contains("operator")) {
+    addOperator(content, display.value);
+    console.log(operationList);
+   } else if (key.classList.contains("decimal") && !isDecimal) {
+    display.value += ".";
+    isDecimal = true;
+   } else if (key.classList.contains("equals")) {
+    
+   }
   });
 });
   
